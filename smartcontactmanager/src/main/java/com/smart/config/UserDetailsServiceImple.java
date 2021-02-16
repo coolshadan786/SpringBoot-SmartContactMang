d4cbfd418856a050c42e4eabe7a3d95ee7b3bd77
coolshadan786 <coolshadan786@gmail.com>
2021-02-16 18:01:57 +0530

    first commit

---
+++ smartcontactmanager/src/main/java/com/smart/config/UserDetailsServiceImple.java
@@ -0,0 +1,28 @@
+package com.smart.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+
+public class UserDetailsServiceImple implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		//How to fetching user by Database
+		User user = userRepository.getUserByUserName(username);
+		
+		if(user==null) {
+			throw new UsernameNotFoundException("Could not found user !!");
+		}
+		CustomUserDatails customUserDatails=new CustomUserDatails(user);
+		return customUserDatails;
+	}
+
+}
